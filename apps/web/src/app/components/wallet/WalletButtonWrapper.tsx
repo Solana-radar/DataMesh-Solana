d3541e9f0fea6057c60b65415b22872fa763440c
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-09-30 16:41:59 +0530

    Fixed : scroll problem

--- apps/web/src/app/components/wallet/WalletButtonWrapper.tsx
+++ apps/web/src/app/components/wallet/WalletButtonWrapper.tsx
@@ -1,32 +1,44 @@
-import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import * as web3 from "@solana/web3.js";
 import * as walletAdapterReact from "@solana/wallet-adapter-react";
 import * as walletAdapterWallets from "@solana/wallet-adapter-wallets";
 import {
-  WalletDisconnectButton,
   WalletModalProvider,
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { clusterApiUrl } from "@solana/web3.js";
 
 const WalletConnectionButton = () => {
   const endpoint = web3.clusterApiUrl("devnet");
   const wallets = [new walletAdapterWallets.PhantomWalletAdapter()];
-  const conneciton = useConnection();
+
+  useEffect(() => {
+    const handleBodyScroll = () => {
+      const modalOpen = document.querySelector(".wallet-adapter-modal-wrapper");
+      if (modalOpen) {
+        document.body.classList.add("scroll-enabled"); // Apply custom scroll class
+      } else {
+        document.body.classList.remove("scroll-enabled"); // Remove scroll class when modal closes
+      }
+    };
+
+    const observer = new MutationObserver(handleBodyScroll);
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   return (
-    <>
-      <walletAdapterReact.ConnectionProvider endpoint={'https://solana-devnet.g.alchemy.com/v2/J0Wh8WUuG5Cm0yeMfDNAqv3xoERG6OQK'}>
-        <walletAdapterReact.WalletProvider wallets={wallets} autoConnect>
-          <WalletModalProvider>
-            <WalletMultiButton className="!bg-helius-orange !rounded-xl hover:!bg-[#161b19] transition-all duration-200" />
-            {/* <WalletDisconnectButton /> */}
-          </WalletModalProvider>
-        </walletAdapterReact.WalletProvider>
-      </walletAdapterReact.ConnectionProvider>
-    </>
+    <walletAdapterReact.ConnectionProvider
+      endpoint="https://solana-devnet.g.alchemy.com/v2/J0Wh8WUuG5Cm0yeMfDNAqv3xoERG6OQK"
+    >
+      <walletAdapterReact.WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton className="!bg-helius-orange !rounded-xl hover:!bg-[#161b19] transition-all duration-200" />
+        </WalletModalProvider>
+      </walletAdapterReact.WalletProvider>
+    </walletAdapterReact.ConnectionProvider>
   );
 };
 
