6f9a77e9a6e606774efe82567d7d47682b56a57d
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-10-05 22:31:25 +0530

    feat - blink post req init

--- apps/web/src/app/api/action/route.ts
+++ apps/web/src/app/api/action/route.ts
@@ -1,18 +1,83 @@
 import {
   ActionGetResponse,
+  ActionPostRequest,
+  ActionPostResponse,
   ACTIONS_CORS_HEADERS,
+  createPostResponse,
 } from "@solana/actions";
+
+import {
+  clusterApiUrl,
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
 
 export const GET = async (req: Request) => {
   const payload: ActionGetResponse = {
-    icon: new URL("/apps/web/src/app/images/invoice.png", new URL(req.url).origin).toString(),
+    icon: new URL(
+      "/web/src/app/images/invoice.png",
+      new URL(req.url).origin
+    ).toString(),
     label: "Donate for DataMesh",
-    description:
-      "Donate so that we can get more data",
+    description: "Donate so that we can get more data",
     title: "DataMesh Donation Blink",
   };
   return Response.json(payload, {
     headers: ACTIONS_CORS_HEADERS,
   });
 };
+
 export const OPTIONS = GET;
+
+export const POST = async (req: Request) => {
+  try {
+    const body: ActionPostRequest = await req.json();
+    let account; // changed
+    try {
+      account = new PublicKey(body.account);
+    } catch (error) {
+      throw "Invalid account provided. It's not a real Pubkey";
+    }
+
+    const connection = new Connection(clusterApiUrl('devnet')) //  change this to devnet before deplpoymnt
+
+    const TO_PIBKEY = new PublicKey(
+      "H71iRgaTaCft1nLZbMiiz6gWB8HaAxsbbZVRYGiWmSUT" // change this to reviver mainnet wallet address
+    );
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: account,
+        lamports: 0.001 * LAMPORTS_PER_SOL,
+        toPubkey: TO_PIBKEY,
+        programId: SystemProgram.programId,
+      })
+    );
+    transaction.feePayer = account;
+    transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
+    const payload: ActionPostResponse = await createPostResponse({
+      fields: {
+        transaction: transaction, // changed
+        message: "Thanks for the donation",
+        type: "transaction", // changed
+      },
+    });
+
+    return Response.json(payload, {
+      headers: ACTIONS_CORS_HEADERS,
+    });
+  } catch (error) {
+    console.log("error", error);
+    let message = "An unknown error happened!";
+    if (typeof error === "string") message === error;
+    return Response.json(
+      {
+        message,
+      },
+      { headers: ACTIONS_CORS_HEADERS }
+    );
+  }
+};
